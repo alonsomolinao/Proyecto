21dc17c39dcd0b10fe7ad0d3c97fcf02fa12e87e
alonsomolinao <34259662+alonsomolinao@users.noreply.github.com>
2017-12-05 17:53:23 -0600

    Limpieza y Creacion de Funciones Void

--- ProyectoCarritoSuper.cpp
+++ ProyectoCarritoSuper.cpp
@@ -2,84 +2,58 @@
 
 using namespace std;
 
-int n;
+int cantidadDeProductos;
+long pesoMaximo;
+int * pos, * valor, * peso, *posf;
+long * ratio, * maxs;
+
+void configurarTienda();
+void ObtenerRatio();
+void MayoraMenorRatio();
+
 
 int main ()
 
 {
-	int l, c, temp, h;
-	int * pos, * valor, * peso, *posf;
-	long * ratio, * maxs;
-	long m, sumapeso;
+	int l, c, h;
+	long sumapeso;
 	
-	cin>>n>>m; //Se lee la cantidad de productos y la capacidad del carrito.
+	configurarTienda();
 	
-	valor = new int[n];
-    peso = new int[n];
-    posf = new int[n];
-    pos = new int[n];
-    ratio = new long[n];
-    maxs = new long[n];
+	valor = new int[cantidadDeProductos];
+    peso = new int[cantidadDeProductos];
+    posf = new int[cantidadDeProductos];
+    pos = new int[cantidadDeProductos];
+    ratio = new long[cantidadDeProductos];
+    maxs = new long[cantidadDeProductos];
 	
 	
-	for (int i=0;i<n;i++) //Se lee el peso y valor de n cantidad de productos.
-	{
-		cin>>peso[i];
-		cin>>valor[i];
-		if (peso[i]==0)
-		{
-			ratio[i]=valor[i]*500;
-		}
-		else
-		{
-		ratio[i]=(valor[i])*100/(peso[i]); // este valor es la relación entre el valor y peso de un producto
-		}
-	}
-cout<<endl;
-	maxs[0]=1000;
+	ObtenerRatio();
 	
-	for (int i=0;i<n;i++)
-	{
-			maxs[i]=ratio[i];
-	}
+	MayoraMenorRatio();
 	
 	
-for(int i=0;i<n;i++)
- {
- for(int j=0;j<n-i-1;j++)
- {
- if(maxs[j]<maxs[j+1])
- {
- temp=maxs[j];
- maxs[j]=maxs[j+1];
- maxs[j+1]=temp;
- }
- }
- }
-	
-	
-	for (int i=0;i<n;i++)
+	for (int i = 0; i < cantidadDeProductos; i++) //Se le asigna un valor de 1000 a la posicion para cambiar este valor mas adelante
 	{
 		pos[i]=1000;
 	}
 	
 	
-	h=0;
 	
-	for (int i=0;i<n;i++)
+	for (int i=0;i<cantidadDeProductos;i++)
 	{
-		for (int j=0;j<n;j++)
+		for (int j=0;j<cantidadDeProductos;j++)
 		{
 			h=0;
-			for (int k=0;k<n;k++)
+			for (int k=0;k<cantidadDeProductos;k++)
 			{
-				if (j!=pos[n-k-1])
+				if (j!=pos[cantidadDeProductos-k-1])
 				{
 					h++;
 				}
 			}
 			
-			if (ratio[j]==maxs[i] && h==n)
+			if (ratio[j]==maxs[i] && h==cantidadDeProductos)
 			{
 				pos[i]=j; //es la pocision en las matrices originales en donde se encuentran los valores de mayor a menor.
 			}
@@ -89,15 +63,15 @@ for(int i=0;i<n;i++)
 	sumapeso=0;
 	l=0;
 	
-	for (int i=0;i<n;i++)
+	for (int i=0;i<cantidadDeProductos;i++)
 	{
 		posf[i]=1000;
 	}
 	
-	for (int i=0;i<n;i++)
+	for (int i=0;i<cantidadDeProductos;i++)
 	{
 		sumapeso+=peso[pos[i]];//se le suma el peso del siguiente producto conveniente
-		if (sumapeso<=m) //si el peso de los productos no excede la capacidad del carrito
+		if (sumapeso<=pesoMaximo) //si el peso de los productos no excede la capacidad del carrito
 		{
 		l++;
 		posf[l]=pos[i];
@@ -111,7 +85,7 @@ for(int i=0;i<n;i++)
 	c=0;
 	
 	
-	for (int i=0;i<n;i++)
+	for (int i=0;i<cantidadDeProductos;i++)
 	{
 		c=0;
 		for (int j=1;j<l+1;j++)
@@ -133,6 +107,66 @@ for(int i=0;i<n;i++)
 	}
 	
 	
+	
+	
 	return 0;
 }
 
+
+void configurarTienda()
+{
+    int n = 0;
+    long m = 0;
+    bool accepted = false;
+    while (!accepted)
+    {
+        cin>>n>>m;
+        if (n <= 20 && m <= 10000)
+        {
+            accepted = true;
+        }
+    }
+    cantidadDeProductos = n;
+    pesoMaximo = m;
+}
+
+void ObtenerRatio()
+{
+	for (int i=0;i<cantidadDeProductos;i++) //Se lee el peso y valor de n cantidad de productos.
+	{
+		cin>>peso[i];
+		cin>>valor[i];
+		if (peso[i]==0)
+		{
+			ratio[i]=valor[i]*500;
+		}
+		else
+		{
+		ratio[i]=(valor[i])*100/(peso[i]); // este valor es la relación entre el valor y peso de un producto
+		}
+	}
+}
+
+void MayoraMenorRatio()
+{
+int temp;
+
+for (int i=0;i<cantidadDeProductos;i++)
+	{
+			maxs[i]=ratio[i];
+	}
+
+
+for(int i=0;i<cantidadDeProductos;i++)
+ 	{
+ 		for(int j=0;j<cantidadDeProductos-i-1;j++)
+ 		{
+ 			if(maxs[j]<maxs[j+1])
+			{
+				temp=maxs[j];
+ 				maxs[j]=maxs[j+1];
+				maxs[j+1]=temp;
+			}
+ 		}
+ 	}
+}
