30993748a64bc24971e4d32cbf0b567a7dd2f798
Demetrio Perez Osorio <Demetrio.Osorio@walmartlabs.com>
2017-12-05 20:53:26 -0800

    LIMPIEZA DE CODIGO

--- Untitled15.cpp
+++ Untitled15.cpp
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <time.h> //Estos dos se requieren para poder generar numeros aleatorios.
 using namespace std;
-// Modalidades
+// Modalidades del programa
 enum Modalidades {
     Primera,
     Segunda,
     Tercera
+};
+//Estructura de datos que engloba los valores de valor y peso.
+struct Producto {
+    int valor;
+    int peso;
 };
 
 // caracteristicas de la tienda
@@ -14,10 +19,12 @@ long pesoMaximo;
 int cantidadDeProductos;
 
 // Arreglos de productos en tiempo de ejecucion
-int * valor, * peso, * posf;
+int  * posf;
 
+Producto * productos;
 // Contadores
 long  sumapeso, sumamax, sumavalor;
+
 int l, sumapesomin, posicion;
 
 // MŽtodos
@@ -25,45 +32,37 @@ void configurarTienda();
 void configurarProductos();
 void inicializarContadores();
 void encontrarMaximos(Modalidades modalidad);
+void imprimirResultados();
+int * inicializarPosiciones();
 
 int main ()
 {
-    int c;
-    long k;
-
     sumamax=0; // Limpieza pendiente
-
     srand(time(NULL));
-
     configurarTienda();
-
-    valor = new int[cantidadDeProductos];
-    peso = new int[cantidadDeProductos];
+    productos = new Producto[cantidadDeProductos];
     posf = new int[cantidadDeProductos];
-
     configurarProductos();
-
     //Todos los contadores empiezan en 0.
     inicializarContadores();
-
     // Encontrar sumaMaxima
     encontrarMaximos(Primera);
-
     sumapesomin = 10000; //El peso minimo empieza siendo mayor a todos los pesos.
-
     //Encontrar combinaci—n con menor peso
     encontrarMaximos(Segunda);
-
     encontrarMaximos(Tercera);
+    imprimirResultados();
+    return 0;
+}
 
+void imprimirResultados()
+{
+    int c = 0;
     cout<<  endl;
-
-    c=0;
-
     for (int i=0;i<cantidadDeProductos;i++)
     {
         c=0;
-        for (k=1;k<posicion+1;k++)
+        for (int k=1;k<posicion+1;k++)
         {
             if (i==posf[k])
             {
@@ -88,8 +87,6 @@ int main ()
         }
 
     }
-
-    return 0;
 }
 
 void configurarTienda()
@@ -113,8 +110,8 @@ void configurarProductos()
 {
     for (int i=0;i<cantidadDeProductos;i++) //Se lee el peso y valor de n cantidad de productos.
     {
-        cin>>peso[i];
-        cin>>valor[i];
+        cin>>productos[i].peso;
+        cin>>productos[i].valor;
     }
 }
 
@@ -136,10 +133,21 @@ long calcularCiclos() {
     return factorial(cantidadDeProductos);
 }
 
+int * inicializarPosiciones()
+{
+    int  * posiciones = new int[cantidadDeProductos];
+    for(int i = 0 ;i < cantidadDeProductos; i++) //se le asigna un valor de 100 a los números
+    {
+        posiciones[i] = 100;
+    }
+    return posiciones;
+}
+
 void encontrarMaximos(Modalidades modalidad)
 {
     long sumatoria;
-    int  * productos = new int[cantidadDeProductos];
+
+    int * posiciones;
     long limit = calcularCiclos();
 
     for (long k = 0; k < limit; k++) //se repite todo este ciclo muchas veces para tener el valor maximo.
@@ -147,33 +155,31 @@ void encontrarMaximos(Modalidades modalidad)
         posicion = 0;
         sumatoria = 0;
         inicializarContadores();
-        for(int i = 0 ;i < cantidadDeProductos; i++) //se le asigna un valor de 100 a los números
-        {
-            productos[i] = 100;
-        }
+        posiciones = inicializarPosiciones();
         for (int i = 0;i < cantidadDeProductos; i++)
         {
             l = 0;
-            productos[i] = rand() % cantidadDeProductos; //se obtiene un numero aleatorio entre 0 y la cantidad de productos que va a indicar la posicion de lo que se agrega.
+            posiciones[i] = rand() % cantidadDeProductos; //se obtiene un numero aleatorio entre 0 y la cantidad de productos que va a indicar la posicion de lo que se agrega.
             for (int j = 0; j < cantidadDeProductos; j++)
             {
-                if (productos[i] != productos[cantidadDeProductos - j - 1])
+                if (posiciones[i] != posiciones[cantidadDeProductos - j - 1])
                 {
                     l++;
                 }
             }
+
             if (l == cantidadDeProductos - 1) //Si este numero no se habia utilizado antes
             {
-                sumapeso += peso[productos[i]];//se le suma el peso del producto en posicion num[i]
+                sumapeso += productos[posiciones[i]].peso;//se le suma el peso del producto en posicion num[i]
                 if (sumapeso <= pesoMaximo) //si el peso de los productos no excede la capacidad del carrito
                 {
                     posicion ++;
-                    sumatoria += valor[productos[i]]; //se le suma el valor de este producto
-                    posf[posicion] = productos[i];
+                    sumatoria += productos[posiciones[i]].valor; //se le suma el valor de este producto
+                    posf[posicion] = posiciones[i];
                 }
                 else
                 {
-                    sumapeso -= peso[productos[i]]; //se resta el peso de ese producto si el carrito ya no tenía la capacidad
+                    sumapeso -= productos[posiciones[i]].peso; //se resta el peso de ese producto si el carrito ya no tenía la capacidad
                 }
             }
         }
@@ -203,5 +209,6 @@ void encontrarMaximos(Modalidades modalidad)
         }
     }
 }
+
 
 
